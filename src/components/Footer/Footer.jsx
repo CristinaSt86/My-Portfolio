745577ee7e72184c41a1199c32c9191aee0f0e34
Cristina Stoian <cristina.stoian@yahoo.com>
2024-06-11 17:10:07 +0200

    impletemted the footer translation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import css from "./Footer.module.css"
+import { useTranslation } from 'react-i18next'
 
 const Footer = () => {
+  const { t } = useTranslation();
   return (
     <footer className={css.footer}>
       <div className={css.container}>
-        <p>&copy; 2024  Cristina Stoian.  All rights reserved.</p>
+        <p>{t('footerText')}</p>
       </div>
     </footer>
   )
