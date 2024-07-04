8f571ecdf2baea6af351c9bae10becabf53aea70
Cristina Stoian <cristina.stoian@yahoo.com>
2024-07-04 13:06:37 +0200

    Minor design adjusments.

--- src/components/LightboxGallery/LightboxGallery.jsx
+++ src/components/LightboxGallery/LightboxGallery.jsx
@@ -1,77 +1,9 @@
-// import React, { useState } from "react";
-// import ImageGallery from "react-image-gallery";
-// import "react-image-gallery/styles/css/image-gallery.css";
-// import Image from "../Image/Image";
-// import css from "../../FigoRest/FigoRest.module.css";
-// import x from "../../images/x.png";
-
-// export const LightboxGallery = ({ images }) => {
-//   const [isOpen, setIsOpen] = useState(false);
-//   const [photoIndex, setPhotoIndex] = useState(0);
-
-//   const galleryImages = images.map((imgSrc) => ({
-//     original: imgSrc,
-//     thumbnail: imgSrc,
-//   }));
-
-//   const handleImageClick = (index) => {
-//     setPhotoIndex(index);
-//     setIsOpen(true);
-//   };
-
-//   return (
-//     <div>
-//       {!isOpen && (
-//         <div className={css.galleryContainer}>
-//           {images.map((image, index) => (
-//             <Image
-//               key={index}
-//               src={image}
-//               alt={`Gallery Image ${index + 1}`}
-//               className={css.imagine}
-//               onClick={() => handleImageClick(index)}
-//             />
-//           ))}
-//         </div>
-//       )}
-
-//       {isOpen && (
-//         <>
-//           <button
-//             onClick={() => setIsOpen(false)}
-//             style={{
-//               borderRadius: "50%",
-//               border: "1px solid #374957",
-//               aspectRatio: "1",
-//               // marginBottom: "0.2rem"
-//             }}
-//           >
-//             <img src={x} className={css.btnImg} alt="exit" />
-//           </button>
-//           <ImageGallery
-//             items={galleryImages}
-//             startIndex={photoIndex}
-//             showThumbnails={true}
-//             onSlide={(currentIndex) => setPhotoIndex(currentIndex)}
-//             infinite={true}
-//             showPlayButton={false}
-//             showFullscreenButton={false}
-//             showIndex={false}
-//             showBullets={true}
-//           />
-//         </>
-//       )}
-//     </div>
-//   );
-// };
-
 import React, { useState } from "react";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import Image from "../Image/Image";
-import css from "./LightboxGallery.module.css"
+import css from "./LightboxGallery.module.css";
 import Modal from "../Modal/Modal";
-// import x from "../../images/x.png";
 
 export const LightboxGallery = ({ images }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -80,8 +12,8 @@ export const LightboxGallery = ({ images }) => {
   const galleryImages = images.map((imgSrc) => ({
     original: imgSrc,
     thumbnail: imgSrc,
-    originalClass: css.imgFit, 
-    thumbnailClass: css.imgFit
+    originalClass: css.imgFit,
+    thumbnailClass: css.imgFit,
   }));
 
   const handleImageClick = (index) => {
@@ -97,7 +29,7 @@ export const LightboxGallery = ({ images }) => {
             key={index}
             src={image}
             alt={`Gallery Image ${index + 1}`}
-            className={css.imagine}
+            className="cursor-pointer hover:opacity-75 rounded-lg shadow-lg transition-transform duration-300 transform hover:scale-105"
             onClick={() => handleImageClick(index)}
           />
         ))}
@@ -119,4 +51,3 @@ export const LightboxGallery = ({ images }) => {
     </div>
   );
 };
-
