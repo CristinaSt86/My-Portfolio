05feca285bcb6a48f8d61ad84de2fee388dc2b8c
Cristina Stoian <cristina.stoian@yahoo.com>
2024-07-09 11:18:26 +0200

    Style Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,6 @@ import BackButton from "../BackButton/BackButton";
 import Navigation from "../Navigation/Navigation";
 import { Link } from "react-router-dom";
 import ThemeToggler from "../ThemeToggler/ThemeToggler";
-import { useLanguage } from "../../LanguageContext";
 import { useTranslation } from "react-i18next";
 import logo from "../../images/l3.svg";
 
